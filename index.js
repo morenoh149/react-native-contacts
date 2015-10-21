98987fac443f5f3da95439c2241acd9e2ed3b8d9
JeanLebrument <jean.lebrument@gmail.com>
2015-10-21 13:32:45 +0200

    Add rx_ prefixed methods to add rx support to methods with completion block

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
 var ReactNative = require('react-native')
-module.exports = ReactNative.NativeModules.Contacts
+var Rx = require('rx');
+var Contacts = ReactNative.NativeModules.Contacts;
+
+function _execAddressBookMethodWithParamsErrorData(func) {
+  return Rx.Observable.create(function(observer) {
+    func(function(error, data) {
+      if (error) {
+        observer.onError(error);
+      } else {
+        observer.onNext(data);
+        observer.onCompleted();
+      }
+    });
+
+    return null;
+  });
+}
+
+function _execAddressBookMethodWithParamError(func) {
+  return Rx.Observable.create(function(observer) {
+    func(function(error) {
+      if (error) {
+        observer.onError(error);
+      } else {
+        observer.onCompleted();
+      }
+    });
+
+    return null;
+  });
+}
+
+Contacts.rx_checkPermission = function() {
+  return _execAddressBookMethodWithParamsErrorData(Contacts.checkPermission);
+};
+
+Contacts.rx_requestPermission = function() {
+  return _execAddressBookMethodWithParamsErrorData(Contacts.requestPermission);
+};
+
+Contacts.rx_getAll = function() {
+  return _execAddressBookMethodWithParamsErrorData(Contacts.getAll);
+};
+
+Contacts.rx_addContact = function() {
+  return _execAddressBookMethodWithParamError(Contacts.addContact);
+}
+
+Contacts.rx_updateContact = function() {
+  return _execAddressBookMethodWithParamError(Contacts.updateContact);
+}
+
+Contacts.rx_deleteContact = function() {
+  return _execAddressBookMethodWithParamError(Contacts.deleteContact);
+}
+
+module.exports = Contacts;

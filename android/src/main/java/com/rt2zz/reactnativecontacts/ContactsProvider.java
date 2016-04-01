d5ba43571140633237e17e90fa8d56c9f643fda9
npomfret <nick-github@snowmonkey.co.uk>
2016-04-01 10:27:01 +0100

    added structured names

--- android/src/main/java/com/rt2zz/reactnativecontacts/ContactsProvider.java
+++ android/src/main/java/com/rt2zz/reactnativecontacts/ContactsProvider.java
@@ -33,6 +33,10 @@ public class ContactsProvider {
                         ContactsContract.Contacts.DISPLAY_NAME,
                         Contactables.PHOTO_URI,
                         ContactsContract.Contacts.HAS_PHONE_NUMBER,
+                        StructuredName.DISPLAY_NAME,
+                        StructuredName.GIVEN_NAME,
+                        StructuredName.MIDDLE_NAME,
+                        StructuredName.FAMILY_NAME,
                         Phone.NUMBER,
                         Phone.TYPE,
                         Phone.LABEL,
@@ -41,8 +45,8 @@ public class ContactsProvider {
                         Email.TYPE,
                         Email.LABEL
                 },
-                ContactsContract.Data.MIMETYPE + "=? OR " + ContactsContract.Data.MIMETYPE + "=?",
-                new String[]{Email.CONTENT_ITEM_TYPE, Phone.CONTENT_ITEM_TYPE},
+                ContactsContract.Data.MIMETYPE + "=? OR " + ContactsContract.Data.MIMETYPE + "=? OR " + ContactsContract.Data.MIMETYPE + "=?",
+                new String[]{Email.CONTENT_ITEM_TYPE, Phone.CONTENT_ITEM_TYPE, StructuredName.CONTENT_ITEM_TYPE},
                 null
         );
 
@@ -59,14 +63,18 @@ public class ContactsProvider {
             String mimeType = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.MIMETYPE));
 
             String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-            if (!TextUtils.isEmpty(name))
-                contact.name = name;
+            if (!TextUtils.isEmpty(name) && TextUtils.isEmpty(contact.displayName))
+                contact.displayName = name;
 
             String photoUri = cursor.getString(cursor.getColumnIndex(Contactables.PHOTO_URI));
             if (!TextUtils.isEmpty(photoUri))
                 contact.photoUri = photoUri;
 
-            if (mimeType.equals(Phone.CONTENT_ITEM_TYPE)) {
+            if (mimeType.equals(StructuredName.CONTENT_ITEM_TYPE)) {
+                contact.givenName = cursor.getString(cursor.getColumnIndex(StructuredName.GIVEN_NAME));
+                contact.middleName = cursor.getString(cursor.getColumnIndex(StructuredName.MIDDLE_NAME));
+                contact.familyName = cursor.getString(cursor.getColumnIndex(StructuredName.FAMILY_NAME));
+            } else if (mimeType.equals(Phone.CONTENT_ITEM_TYPE)) {
                 String phoneNumber = cursor.getString(cursor.getColumnIndex(Phone.NUMBER));
                 int type = cursor.getInt(cursor.getColumnIndex(Phone.TYPE));
 
@@ -85,9 +93,7 @@ public class ContactsProvider {
                         label = "other";
                 }
                 contact.phones.add(new Contact.Item(label, phoneNumber));
-            }
-
-            if (mimeType.equals(Email.CONTENT_ITEM_TYPE)) {
+            } else if (mimeType.equals(Email.CONTENT_ITEM_TYPE)) {
                 String email = cursor.getString(cursor.getColumnIndex(Email.ADDRESS));
                 int type = cursor.getInt(cursor.getColumnIndex(Email.TYPE));
 
@@ -126,7 +132,10 @@ public class ContactsProvider {
 
     private static class Contact {
         private final int contactId;
-        private String name;
+        private String displayName;
+        private String givenName = "";
+        private String middleName = "";
+        private String familyName = "";
         private String photoUri;
         private List<Item> emails = new ArrayList<>();
         private List<Item> phones = new ArrayList<>();
@@ -138,9 +147,9 @@ public class ContactsProvider {
         public WritableMap toMap() {
             WritableMap contact = Arguments.createMap();
             contact.putInt("recordID", contactId);
-            contact.putString("givenName", name);
-            contact.putString("middleName", "");
-            contact.putString("familyName", "");
+            contact.putString("givenName", TextUtils.isEmpty(givenName) ? displayName : givenName);
+            contact.putString("middleName", middleName);
+            contact.putString("familyName", familyName);
             contact.putString("thumbnailPath", photoUri == null ? "" : photoUri);
 
             WritableArray phoneNumbers = Arguments.createArray();

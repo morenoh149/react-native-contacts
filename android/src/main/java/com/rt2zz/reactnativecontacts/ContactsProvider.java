9d3df09bac2520362847a4ad66174ba0cf878c3e
Takeru Chuganji <chu@ganzy.jp>
2019-06-24 13:36:53 -0700

    Android X migration

--- android/src/main/java/com/rt2zz/reactnativecontacts/ContactsProvider.java
+++ android/src/main/java/com/rt2zz/reactnativecontacts/ContactsProvider.java
@@ -4,7 +4,7 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 

morenoh149/react-native-contacts

commit a3e64f6b00555e5ca593409e8bb2c871dc6a57e8

Fixes crash bug when getting all contacts if defaultContainerIdentifier was null (by removing redundant code).

ios/RCTContacts/RCTContacts.m
@@ -262,11 +262,6 @@ RCT_EXPORT_METHOD(getCount:(RCTResponseSenderBlock) callback)
                            withCallback:(RCTResponseSenderBlock) callback
 {
     NSMutableArray *contacts = [[NSMutableArray alloc] init];
-
-    NSError* contactError;
-    [contactStore containersMatchingPredicate:[CNContainer predicateForContainersWithIdentifiers: @[contactStore.defaultContainerIdentifier]] error:&contactError];
-
-
     NSMutableArray *keysToFetch = [NSMutableArray arrayWithArray: @[
         CNContactEmailAddressesKey,
         CNContactPhoneNumbersKey,

commit 1f73c767d3060bc2c87335c8bb04ae431999a0a9

Fix CD/CI - Update RCTContacts.m
Looks like https://github.com/morenoh149/react-native-contacts/commit/a3e64f6b00555e5ca593409e8bb2c871dc6a57e8 accidentally removed `contactError` which is still used.

ios/RCTContacts/RCTContacts.m
@@ -285,6 +285,7 @@ RCT_EXPORT_METHOD(getCount:(RCTResponseSenderBlock) callback)
     }
 
     CNContactFetchRequest * request = [[CNContactFetchRequest alloc]initWithKeysToFetch:keysToFetch];
+    NSError* contactError;
     BOOL success = [contactStore enumerateContactsWithFetchRequest:request error:&contactError usingBlock:^(CNContact * __nonnull contact, BOOL * __nonnull stop){
         NSDictionary *contactDict = [self contactToDictionary: contact withThumbnails:withThumbnails];
         [contacts addObject:contactDict];
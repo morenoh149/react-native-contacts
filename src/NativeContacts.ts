bc4ab8c9fb0c3763780cffaef7ddd6e85ae8eb98
Ahmed Hamed <ahmedhamed7728@gmail.com>
2024-11-25 18:32:57 +0200

    fix: remove pick from addGroup

--- src/NativeContacts.ts
+++ src/NativeContacts.ts
@@ -26,7 +26,7 @@ export interface Spec extends TurboModule {
   getGroup: (identifier: string) => Promise<Group | null>;
   deleteGroup(identifier: string): Promise<boolean>;
   updateGroup(identifier: string, groupData: Object): Promise<Group>;
-  addGroup(group: Pick<Group, "name">): Promise<Group>;
+  addGroup(group: Object): Promise<Group>;
   contactsInGroup(identifier: string): Promise<Contact[]>;
   addContactsToGroup(
     groupIdentifier: string,
